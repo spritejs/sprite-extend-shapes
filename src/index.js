e4799a623c832549af53e39f38039555d1d6cd44
zhaoxu <bken2016@163.com>
2018-07-18 20:26:06 +0800

    :art: add star.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import Polyline from './polyline'
 import Polygon from './polygon'
 import Triangle from './triangle'
 import Rect from './rect'
+import Star from './star'
 
-export {
-  Shape,
-  Polyline,
-  Polygon,
-  Triangle,
-  Rect
-}
+export { Shape, Polyline, Polygon, Triangle, Rect, Star }
---
+++ src/star.js
@@ -0,0 +1,59 @@
+import Shape from './shape'
+import Polygon from './polygon'
+import { utils } from 'sprite-core'
+
+const { attr, parseColorString, findColor } = utils
+
+class PolygonAttr extends Shape.Attr {
+  constructor(subject) {
+    super(subject)
+    this.setDefault({
+      angles: 5
+    })
+  }
+
+  @attr
+  set angles(num) {
+    this.set('angles', num)
+  }
+
+  @attr
+  set radius(val) {
+    // 圆半径
+    this.set('radius', val)
+  }
+}
+
+function calculateStarN(sides) {
+  return function(x, y, radius) {
+    let sideIndentRadius = radius * 0.5
+    let radAngle = -Math.PI / 2
+    let radAlpha = (Math.PI * 2) / sides / 2
+
+    const points = []
+
+    for (let i = 1; i <= sides * 2; i++) {
+      let rad = radAlpha * i + radAngle
+      let len = i % 2 ? sideIndentRadius : radius
+      let xPos = x + Math.cos(rad) * len
+      let yPos = y + Math.sin(rad) * len
+
+      points.push([xPos, yPos])
+    }
+
+    return points
+  }
+}
+
+class Star extends Polygon {
+  static Attr = PolygonAttr
+
+  get points() {
+    const [x, y] = this.attr('pos')
+    const radius = this.attr('radius')
+
+    return calculateStarN(this.attr('angles'))(x, y, radius)
+  }
+}
+
+export default Star
