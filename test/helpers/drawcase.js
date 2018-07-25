80dfed82b74695bee3d73673b208c30295642b22
zhaoxu <bken2016@163.com>
2018-07-25 12:15:23 +0800

    :bug: fix img path

--- test/helpers/drawcase.js
+++ test/helpers/drawcase.js
@@ -1,16 +1,20 @@
-const fs = require('fs');
-const {createCanvas} = require('canvas');
-const Layer = require('sprite-core').Layer;
+const fs = require("fs");
+const path = require("path");
+const { createCanvas } = require("canvas");
+const Layer = require("sprite-core").Layer;
 
-module.exports = async function (caseId, size, handler, noEllipse = false) {
+module.exports = async function(caseId, size, handler, noEllipse = false) {
   const canvas = createCanvas(...size);
-  const context = canvas.getContext('2d');
+  const context = canvas.getContext("2d");
 
   if (noEllipse) {
-    context.ellipse = null
+    context.ellipse = null;
   }
 
-  const layer = new Layer({context});
+  const layer = new Layer({ context });
   await handler(layer, size);
-  fs.writeFileSync(`../img/${caseId}.png`, canvas.toBuffer());
+  fs.writeFileSync(
+    path.resolve(__dirname, `../img/${caseId}.png`),
+    canvas.toBuffer()
+  );
 };
